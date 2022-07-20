18e578cc94d2f1648c58c6cfef98042cd8aebf9f
Quyet Tran <tranminhquyet20@gmail.com>
2022-07-21 04:45:31 +0700

    boiler plate gymweb

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    debugScreens: {
+      position: ['top','left']
+    },
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
+}
